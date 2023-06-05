cd21a2ed5d4bed07a0bd6b137fd9b542b66ab87d
Sergey <sknyazev@plotpad.ru>
2023-06-05 12:21:55 +0300

    added window implementation

--- src/main/kotlin/HttpClient.kt
+++ src/main/kotlin/HttpClient.kt
@@ -2,8 +2,10 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.*
 import io.ktor.client.engine.js.*
 import io.ktor.client.request.*
+import kotlinx.browser.window
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.await
 import kotlinx.coroutines.promise
 import kotlin.js.Promise
 
@@ -17,6 +19,18 @@ fun getIp(): Promise<String> {
         TestHttpClient.get("http://ip.jsontest.com/")
     }
 }
+
+@OptIn(ExperimentalJsExport::class)
+@JsExport
+fun getIpUsingWindow(): Promise<String> {
+    return scope.promise {
+        JSON.stringify(
+            Promise.resolve(window.fetch("http://ip.jsontest.com/").then { response ->
+                response.json()
+            }.then { it }).await())
+    }
+}
+
 object TestHttpClient {
     private val httpClient = HttpClient(Js.create())
 
